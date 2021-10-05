2c69d80d36c0651b156ddd1a2d4fca2187a42704
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-10-05 11:05:12 +0200

    Finalisation

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-const port = process.env.DB_PORT || 8080;
+const port = process.env.DB_PORT || ' https://argonautes-mb.herokuapp.com/';
 
 server.listen(port, () => {
     console.log(`Listening on port : ${port}`)
